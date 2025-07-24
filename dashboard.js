8a52c13ec195f206f5009697d01ced13246b83a4
Ali Mehri <alimehridev@gmail.com>
2025-07-24 20:26:59 +0330

    removing a log from log list is functional

--- dashboard.js
+++ dashboard.js
@@ -5,6 +5,29 @@ function getQueryParam(param) {
   return urlParams.get(param);
 }
 
+function removeKeywordFromStorage(origin, href, keywordToRemove) {
+  chrome.storage.local.get("keywords_by_origin", (data) => {
+    const all = data.keywords_by_origin || {};
+    if (!all[origin] || !all[origin][href]) return;
+    all[origin][href] = all[origin][href].filter(
+      (kw) => kw !== keywordToRemove
+    );
+    if (all[origin][href].length === 0) {
+      delete all[origin][href];
+    }
+    if (Object.keys(all[origin]).length === 0) {
+      delete all[origin];
+    }
+    chrome.storage.local.set({ keywords_by_origin: all });
+  });
+}
+
+
+function removeLogFunction(page, origin, pageURL, keyword){
+  page.remove()
+  removeKeywordFromStorage(origin, pageURL, keyword)
+}
+
 async function loadOriginData(origin) {
   document.getElementById("origin").textContent = `🔗 Origin: ${origin}`;
 
@@ -30,6 +53,13 @@ async function loadOriginData(origin) {
       kw.className = "keyword";
       kw.textContent = keyword;
       pageDiv.appendChild(kw);
+      let remove_log_btn = document.createElement("button")
+      remove_log_btn.classList.add("removeLogBtn")
+      remove_log_btn.innerText = "remove"
+      pageDiv.appendChild(remove_log_btn)
+      remove_log_btn.addEventListener("click", () => {
+        removeLogFunction(pageDiv, origin, pageURL, keyword)
+      })
       dataDiv.appendChild(pageDiv);
     });
 
